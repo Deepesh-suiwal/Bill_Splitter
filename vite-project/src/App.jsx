1591618f288776e37bc8814a83654fab06dd1a7a
Deepesh <sainideepesh12@gmail.com>
2025-04-04 18:33:29 +0530

    done CSS

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -98,57 +98,55 @@ function App() {
 
   return (
     <>
-      <div className="wrapper">
+      <div className="parent">
         <h1 className="heading">Bill Splitter</h1>
-        <div className="parent">
-          <div className="BillSplitter">
-            <div className="inputs">
-              <div className="amount">
-                <h2>Bill</h2>
-                <input
-                  type="number"
-                  placeholder="Enter your amount"
-                  value={amount}
-                  onChange={(e) => setAmount(e.target.value)}
-                />
-              </div>
-              <div className="PercentTip">
-                <h3>Select Tip</h3>
-                <button onClick={() => handleTipClick(5)}>5%</button>
-                <button onClick={() => handleTipClick(10)}>10%</button>
-                <button onClick={() => handleTipClick(15)}>15%</button>
-                <button onClick={() => handleTipClick(25)}>25%</button>
-                <button onClick={() => handleTipClick(50)}>50%</button>
-                <button onClick={() => handleTipClick(75)}>75%</button>
-              </div>
-              <div className="customTip">
-                <input
-                  type="number"
-                  placeholder="Custom Tip in Percent"
-                  value={customTip}
-                  onChange={(e) => handleCustomTip(e)}
-                />
-              </div>
-              <div className="totalPerson">
-                <input
-                  type="number"
-                  placeholder="Number of People"
-                  value={numPeople}
-                  onChange={(e) => handleNumPeople(e)}
-                />
-              </div>
-
-              <button onClick={calculateBill}>Generate Bill</button>
+        <div className="BillSplitter">
+          <div className="inputs">
+            <div className="amount">
+              <h2>Bill</h2>
+              <input
+                type="number"
+                placeholder="Enter your amount"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+            </div>
+            <div className="PercentTip">
+              <h3>Select Tip</h3>
+              <button onClick={() => handleTipClick(5)}>5%</button>
+              <button onClick={() => handleTipClick(10)}>10%</button>
+              <button onClick={() => handleTipClick(15)}>15%</button>
+              <button onClick={() => handleTipClick(25)}>25%</button>
+              <button onClick={() => handleTipClick(50)}>50%</button>
+              <button onClick={() => handleTipClick(75)}>75%</button>
+            </div>
+            <div className="customTip">
+              <input
+                type="number"
+                placeholder="Custom Tip in Percent"
+                value={customTip}
+                onChange={(e) => handleCustomTip(e)}
+              />
+            </div>
+            <div className="totalPerson">
+              <input
+                type="number"
+                placeholder="Number of People"
+                value={numPeople}
+                onChange={(e) => handleNumPeople(e)}
+              />
             </div>
 
-            <div className="final">
-              <h1>Tip Amount: {finalTip}</h1>
-              <h1>Total: {total}</h1>
-              <h1>Each Person's Bill: {perPerson}</h1>
-              <button className="ResetButton" onClick={HandleReset}>
-                Reset
-              </button>
-            </div>
+            <button onClick={calculateBill}>Generate Bill</button>
+          </div>
+
+          <div className="final">
+            <h1>Tip Amount: {finalTip}</h1>
+            <h1>Total: {total}</h1>
+            <h1>Each Person's Bill: {perPerson}</h1>
+            <button className="ResetButton" onClick={HandleReset}>
+              Reset
+            </button>
           </div>
         </div>
       </div>
